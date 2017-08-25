8b680232d456d13ab7eb1828a92d9ce26ea6af33
Yeongjin Lee <yeongjinnn@gmail.com>
2017-08-25 09:35:17 +0900

    Add rendering url.

--- index.js
+++ index.js
@@ -3,24 +3,47 @@ const createRenderer = require('./renderer');
 
 const port = process.env.PORT || 3000;
 
-createRenderer().then((render) => {
+const app = express();
+
+// Configure.
+app.disable('x-powered-by');
+
+let render = null;
+
+// Index page.
+app.get('/', (req, res) => {
+  res.redirect('/render');
+});
+
+// Render url.
+app.get('/render', async (req, res, next) => {
+  if (!req.query.url) {
+    return res.status(400).send('Search with url parameter. For eaxample, ?url=http://yourdomain');
+  }
+
+  try {
+    const html = await render(req.query.url);
+    res.status(200).send(html);
+  } catch (e) {
+    next(e);
+  }
+});
+
+// Page not found.
+app.use((req, res) => {
+  res.status(404).send('Page not found.');
+});
+
+// Error page.
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Oops, An expected error seems to have occurred.');
+});
+
+// Create renderer and start server.
+createRenderer().then((createdRender) => {
+  render = createdRender;
   console.info('Initialized renderer.');
-
-  const app = express();
-
-  app.use(async (req, res) => {
-    if (!req.query.url) {
-      return res.status(400).send('Search with url parameter. For eaxample, ?url=http://yourdomain');
-    }
-
-    try {
-      const html = await render(req.query.url);
-      res.status(200).send(html);
-    } catch (e) {
-      console.error(e);
-      res.status(500).send('Oops, An expected error seems to have occurred.');
-    }
-  });
 
   app.listen(port, () => {
     console.info(`Listen port on ${port}.`);
