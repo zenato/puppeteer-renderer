bd383f087fd2ec0b574b19b13aa1d4c81a652b6b
Yeongjin Lee <yeongjinnn@gmail.com>
2017-08-30 11:04:00 +0900

    Create express middleware.

---
+++ middleware/README.md
@@ -0,0 +1,37 @@
+#puppeteer-renderer-middleware
+
+An Express middleware for [puppeteer-renderer](http://github.com/zenato/puppeteer-renderer).
+
+
+##Usage
+
+```bash
+$ npm install --save express puppeteer-renderer-middleware
+```
+
+```js
+const express = require('express');
+const puppeteerRenderer = require('puppeteer-renderer-middleware');
+
+const app = express();
+
+app.use(puppeteerRenderer({
+  url: 'http://puppeteer-renderer/render',
+  // userAgentPattern: /My-Custom-Agent/i,
+  // excludeUrlPattern: /*.html$/i
+  // timeout: 30 * 1000,
+}));
+
+// other codes..
+
+app.listen(8080);
+```
+
+##Configuration
+
+| Property | Default | Description |
+| -------- | ------- | ----------- |
+| `url` | *Required* | puppeteer-renderer public url |
+| `userAgentPattern` | Match user agent pattern |
+| `excludeUrlPattern` | Exclude url pattern |
+| `timeout` | `10 * 1000` | Timeout ms |
---
+++ middleware/index.js
@@ -0,0 +1,59 @@
+const axios = require('axios');
+
+const botUserAgents = [
+  'W3C_Validator',
+  'baiduspider',
+  'bingbot',
+  'embedly',
+  'facebookexternalhit',
+  'linkedinbo',
+  'outbrain',
+  'pinterest',
+  'quora link preview',
+  'rogerbo',
+  'showyoubot',
+  'slackbot',
+  'twitterbot',
+  'vkShare',
+];
+
+const staticFileExtensions = [
+  'ai', 'avi', 'css', 'dat', 'dmg', 'doc', 'doc', 'exe', 'flv',
+  'gif', 'ico', 'iso', 'jpeg', 'jpg', 'js', 'less', 'm4a', 'm4v',
+  'mov', 'mp3', 'mp4', 'mpeg', 'mpg', 'pdf', 'png', 'ppt', 'psd',
+  'rar', 'rss', 'svg', 'swf', 'tif', 'torrent', 'ttf', 'txt', 'wav',
+  'wmv', 'woff', 'xls', 'xml', 'zip',
+];
+
+let isRender = false;
+
+module.exports = function (options) {
+  if (!options || !options.url) {
+    throw new Error('Must set url.');
+  }
+
+  let rendererUrl = options.url;
+
+  const userAgentPattern = options.userAgentPattern || new RegExp(botUserAgents.join('|'), 'i');
+  const excludeUrlPattern = options.excludeUrlPattern || new RegExp(`\\.(${staticFileExtensions.join('|')})$`, 'i');
+  const timeout = options.timeout || (10 * 1000);
+
+  return function (req, res, next) {
+    if (isRender) return next();
+    isRender = true;
+
+    if (!userAgentPattern.test(req.headers['user-agent']) || excludeUrlPattern.test(req.path)) {
+      return next();
+    }
+
+    const requestUrl = encodeURIComponent(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    let renderUrl = `${rendererUrl}?url=${requestUrl}`;
+
+    axios.get(renderUrl, { timeout })
+      .then(({ data }) => res.send(data))
+      .catch(e => next(e));
+  };
+};
+
+exports.botUserAgents = botUserAgents;
+exports.staticFileExtensions = staticFileExtensions;
