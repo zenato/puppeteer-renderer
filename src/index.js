1353aa20b5534b40d10a4de88ace8c7f4793fc61
Yeongjin Lee <yeongjinnn@gmail.com>
2020-11-23 19:12:46 +0900

    Refactor renderer codes

--- src/index.js
+++ src/index.js
@@ -1,102 +1,110 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const qs = require('qs')
-const { URL } = require('url')
-const contentDisposition = require('content-disposition')
-const createRenderer = require('./renderer')
+const express = require("express");
+const qs = require("qs");
+const { URL } = require("url");
+const contentDisposition = require("content-disposition");
+const createRenderer = require("./renderer");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-let renderer = null
+let renderer = null;
 
 // Configure.
-app.set('query parser', s => qs.parse(s, { allowDots: true }))
-app.disable('x-powered-by')
+app.set("query parser", (s) => qs.parse(s, { allowDots: true }));
+app.disable("x-powered-by");
 
 // Render url.
 app.use(async (req, res, next) => {
-  let { url, type, filename, ...options } = req.query
+  let { url, type, filename, ...options } = req.query;
 
   if (!url) {
-    return res.status(400).send('Search with url parameter. For eaxample, ?url=http://yourdomain')
+    return res
+      .status(400)
+      .send("Search with url parameter. For eaxample, ?url=http://yourdomain");
   }
 
-  if (!url.includes('://')) {
-    url = `http://${url}`
+  if (!url.includes("://")) {
+    url = `http://${url}`;
   }
 
   try {
     switch (type) {
-      case 'pdf':
-        const urlObj = new URL(url)
+      case "pdf":
+        const urlObj = new URL(url);
         if (!filename) {
-          filename = urlObj.hostname
-          if (urlObj.pathname !== '/') {
-            filename = urlObj.pathname.split('/').pop()
-            if (filename === '') filename = urlObj.pathname.replace(/\//g, '')
-            const extDotPosition = filename.lastIndexOf('.')
-            if (extDotPosition > 0) filename = filename.substring(0, extDotPosition)
+          filename = urlObj.hostname;
+          if (urlObj.pathname !== "/") {
+            filename = urlObj.pathname.split("/").pop();
+            if (filename === "") filename = urlObj.pathname.replace(/\//g, "");
+            const extDotPosition = filename.lastIndexOf(".");
+            if (extDotPosition > 0)
+              filename = filename.substring(0, extDotPosition);
           }
         }
-        if(!filename.toLowerCase().endsWith('.pdf')) {
-          filename += '.pdf';
+        if (!filename.toLowerCase().endsWith(".pdf")) {
+          filename += ".pdf";
         }
-        const { contentDispositionType, ...pdfOptions } = options
-        const pdf = await renderer.pdf(url, pdfOptions)
+        const { contentDispositionType, ...pdfOptions } = options;
+        const pdf = await renderer.pdf(url, pdfOptions);
         res
           .set({
-            'Content-Type': 'application/pdf',
-            'Content-Length': pdf.length,
-            'Content-Disposition': contentDisposition(filename, { type: contentDispositionType || 'attachment' }),
+            "Content-Type": "application/pdf",
+            "Content-Length": pdf.length,
+            "Content-Disposition": contentDisposition(filename, {
+              type: contentDispositionType || "attachment",
+            }),
           })
-          .send(pdf)
-        break
+          .send(pdf);
+        break;
 
-      case 'screenshot':
-        const { screenshotType, buffer } = await renderer.screenshot(url, options)
+      case "screenshot":
+        const { screenshotType, buffer } = await renderer.screenshot(
+          url,
+          options
+        );
         res
           .set({
-            'Content-Type': `image/${screenshotType}`,
-            'Content-Length': buffer.length,
+            "Content-Type": `image/${screenshotType}`,
+            "Content-Length": buffer.length,
           })
-          .send(buffer)
-        break
+          .send(buffer);
+        break;
 
       default:
-        const html = await renderer.render(url, options)
-        res.status(200).send(html)
+        const html = await renderer.html(url, options);
+        res.status(200).send(html);
     }
   } catch (e) {
-    next(e)
+    next(e);
   }
-})
+});
 
 // Error page.
 app.use((err, req, res, next) => {
-  console.error(err)
-  res.status(500).send('Oops, An expected error seems to have occurred.')
-})
+  console.error(err);
+  res.status(500).send("Oops, An expected error seems to have occurred.");
+});
 
 // Create renderer and start server.
 createRenderer({
   ignoreHTTPSErrors: !!process.env.IGNORE_HTTPS_ERRORS,
 })
-  .then(createdRenderer => {
-    renderer = createdRenderer
-    console.info('Initialized renderer.')
+  .then((createdRenderer) => {
+    renderer = createdRenderer;
+    console.info("Initialized renderer.");
 
     app.listen(port, () => {
-      console.info(`Listen port on ${port}.`)
-    })
+      console.info(`Listen port on ${port}.`);
+    });
   })
-  .catch(e => {
-    console.error('Fail to initialze renderer.', e)
-  })
+  .catch((e) => {
+    console.error("Fail to initialze renderer.", e);
+  });
 
 // Terminate process
-process.on('SIGINT', () => {
-  process.exit(0)
-})
+process.on("SIGINT", () => {
+  process.exit(0);
+});
--- src/renderer.js
+++ src/renderer.js
@@ -1,114 +1,139 @@
-'use strict'
+"use strict";
 
-const puppeteer = require('puppeteer')
-const waitForAnimations = require('./wait-for-animations')
+const puppeteer = require("puppeteer");
+const waitForAnimations = require("./wait-for-animations");
 
 class Renderer {
   constructor(browser) {
-    this.browser = browser
+    this.browser = browser;
   }
 
-  async createPage(url, options = {}) {
-    const { timeout, waitUntil, credentials, emulateMedia } = options
-    const page = await this.browser.newPage()
-    if (emulateMedia) {
-      await page.emulateMedia(emulateMedia)
-    }
-
-    if (credentials) {
-      await page.authenticate(credentials)
-    }
-
-    await page.goto(url, {
-      timeout: Number(timeout) || 30 * 1000,
-      waitUntil: waitUntil || 'networkidle2',
-    })
-    return page
-  }
-
-  async render(url, options = {}) {
-    let page = null
+  async html(url, options = {}) {
+    let page = null;
     try {
-      const { timeout, waitUntil, credentials } = options
-      page = await this.createPage(url, { timeout, waitUntil, credentials })
-      const html = await page.content()
-      return html
+      const { timeout, waitUntil, credentials } = options;
+      page = await this.createPage(url, { timeout, waitUntil, credentials });
+      const html = await page.content();
+      return html;
     } finally {
-      if (page) {
-        await page.close()
-      }
+      this.closePage(page);
     }
   }
 
   async pdf(url, options = {}) {
-    let page = null
+    let page = null;
     try {
-      const { timeout, waitUntil, credentials, emulateMedia, ...extraOptions } = options
-      page = await this.createPage(url, { timeout, waitUntil, credentials, emulateMedia: emulateMedia || 'print' })
+      const {
+        timeout,
+        waitUntil,
+        credentials,
+        emulateMedia,
+        ...extraOptions
+      } = options;
+      page = await this.createPage(url, {
+        timeout,
+        waitUntil,
+        credentials,
+        emulateMedia: emulateMedia || "print",
+      });
 
-      const { scale = 1.0, displayHeaderFooter, printBackground, landscape } = extraOptions
+      const {
+        scale = 1.0,
+        displayHeaderFooter,
+        printBackground,
+        landscape,
+      } = extraOptions;
       const buffer = await page.pdf({
         ...extraOptions,
         scale: Number(scale),
-        displayHeaderFooter: displayHeaderFooter === 'true',
-        printBackground: printBackground === 'true',
-        landscape: landscape === 'true',
-      })
-      return buffer
+        displayHeaderFooter: displayHeaderFooter === "true",
+        printBackground: printBackground === "true",
+        landscape: landscape === "true",
+      });
+      return buffer;
     } finally {
-      if (page) {
-        await page.close()
-      }
+      this.closePage(page);
     }
   }
 
   async screenshot(url, options = {}) {
-    let page = null
+    let page = null;
     try {
-      const { timeout, waitUntil, credentials, ...extraOptions } = options
-      page = await this.createPage(url, { timeout, waitUntil, credentials })
+      const { timeout, waitUntil, credentials, ...extraOptions } = options;
+      page = await this.createPage(url, { timeout, waitUntil, credentials });
       page.setViewport({
         width: Number(extraOptions.width || 800),
         height: Number(extraOptions.height || 600),
-      })
+      });
 
-      const { fullPage, omitBackground, screenshotType, quality, ...restOptions } = extraOptions
+      const {
+        fullPage,
+        omitBackground,
+        screenshotType,
+        quality,
+        ...restOptions
+      } = extraOptions;
       let screenshotOptions = {
         ...restOptions,
-        type: screenshotType || 'png',
+        type: screenshotType || "png",
         quality:
-          Number(quality) || (screenshotType === undefined || screenshotType === 'png' ? 0 : 100),
-        fullPage: fullPage === 'true',
-        omitBackground: omitBackground === 'true',
-      }
+          Number(quality) ||
+          (screenshotType === undefined || screenshotType === "png" ? 0 : 100),
+        fullPage: fullPage === "true",
+        omitBackground: omitBackground === "true",
+      };
 
-      const animationTimeout = Number(options.animationTimeout || 0)
+      const animationTimeout = Number(options.animationTimeout || 0);
       if (animationTimeout > 0) {
-        await waitForAnimations(page, screenshotOptions, animationTimeout)
+        await waitForAnimations(page, screenshotOptions, animationTimeout);
       }
 
-      const buffer = await page.screenshot(screenshotOptions)
+      const buffer = await page.screenshot(screenshotOptions);
       return {
         screenshotType,
         buffer,
-      }
+      };
     } finally {
-      if (page) {
-        await page.close()
-      }
+      this.closePage(page);
+    }
+  }
+
+  async createPage(url, options = {}) {
+    const { timeout, waitUntil, credentials, emulateMedia } = options;
+    const page = await this.browser.newPage();
+    if (emulateMedia) {
+      await page.emulateMedia(emulateMedia);
+    }
+
+    if (credentials) {
+      await page.authenticate(credentials);
+    }
+
+    await page.goto(url, {
+      timeout: Number(timeout) || 30 * 1000,
+      waitUntil: waitUntil || "networkidle2",
+    });
+    return page;
+  }
+
+  async closePage(page) {
+    if (page) {
+      try {
+        await page.close();
+      } catch (e) {}
     }
   }
 
   async close() {
-    await this.browser.close()
+    await this.browser.close();
   }
 }
 
 async function create(options = {}) {
   const browser = await puppeteer.launch(
-    Object.assign({ args: ['--no-sandbox'] }, options)
-  )
-  return new Renderer(browser)
+    Object.assign({ args: ["--no-sandbox"] }, options)
+  );
+  return new Renderer(browser);
 }
 
-module.exports = create
+module.exports = create;
